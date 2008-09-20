5bd1005c7be9c5201d8c39959d80af64490ba5bf
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2008-09-20 14:27:16 +0100

    Sundry modifications
    Added comment about another caveat.
    
    In MainMenu.xib, Interface Builder removes
    the partial class description; it now has the complete description. Removed
    Expanded Items Autosaver. It WAS the outline view's data source. The new data
    source points at the app delegate.

--- Abstract_Tree_Mark_II_AppDelegate.m
+++ Abstract_Tree_Mark_II_AppDelegate.m
@@ -26,6 +26,7 @@
 		[NSApp presentError:error];
 	}
 	// Watch out for this! Make sure that the data is available before enabling auto-save for expanded items. You can switch it on within the nib. Problem though is that the data store must be available at nib awaking time! Otherwise, how can the auto-saving of expanded items compare against existing items in order to determine whether or not they should be restored as expanded or not.
+	// There's another caveat. You need to let the bindings make the necessary connections first, before connecting the Core Data context and persistent store coordinator to the store. In other words, the store must be added last. Here is a good place. Application-did-finish-launching occurs after all nib awaking methods, after bindings have been established. Hence, when the store gets added, the outline view immediately sees the contents through its bindings. Otherwise the outline-view expanded items auto-saver cannot see the items at all.
 	[outlineView setAutosaveExpandedItems:YES];
 }
 
