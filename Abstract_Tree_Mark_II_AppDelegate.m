080fa9f9dcea37c1426ce1c95e1a378b20769a63
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2008-11-21 17:02:52 +0000

    New data model, sort descriptor, persistent ordering of nodes
    The data model for Node has a new attribute, a 32-bit integer called order for
    storing Node order!
    
    Use traditional Objective-C message-sending syntax rather than the new
    Objective-C 2.0 property accessor syntax. It's just a matter of style.
    
    Sets the tree controller's sort descriptors. There's just the one, order by
    "order" key ascending.
    
    Drop validation tests for malignant drops, those that might drop a parent on top
    of its child. You cannot do that.
    
    Accepting drops method now moves the drag-source to the end of the child nodes
    when dropping on top of an item. Re-order the represented objects by re-writing
    their "order" properties according to the tree controller's index paths for each
    item standing for the corresponding represented object. This special new
    behaviour exists as in category upon the NSTreeController class, part of
    RRAppKit framework. Perhaps, though, it properly belongs to NSTreeNode. Though
    that would make assumptions about the tree nodes, e.g. that they all have
    represented objects and all such objects have the ordering key.

--- Abstract_Tree_Mark_II_AppDelegate.m
+++ Abstract_Tree_Mark_II_AppDelegate.m
@@ -21,16 +21,36 @@ static NSString *kAbstractTreeNodeType = @"AbstractTreeNodeType";
 	NSURL *url = [NSURL appSupportFolderFileURLWithPathComponent:@"Abstract Tree Mark II.xml" error:&error];
 	if (url)
 	{
-		[[RRCoreDataStack sharedInstance].coordinator addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error];
+		[[[RRCoreDataStack sharedInstance] coordinator] addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error];
 	}
 	if (error)
 	{
 		[NSApp presentError:error];
 	}
-	// Watch out for this! Make sure that the data is available before enabling auto-save for expanded items. You can switch it on within the nib. Problem though is that the data store must be available at nib awaking time! Otherwise, how can the auto-saving of expanded items compare against existing items in order to determine whether or not they should be restored as expanded or not.
-	// There's another caveat. You need to let the bindings make the necessary connections first, before connecting the Core Data context and persistent store coordinator to the store. In other words, the store must be added last. Here is a good place. Application-did-finish-launching occurs after all nib awaking methods, after bindings have been established. Hence, when the store gets added, the outline view immediately sees the contents through its bindings. Otherwise the outline-view expanded items auto-saver cannot see the items at all.
+	
+	// Watch out for this! Make sure that the data is available before enabling
+	// auto-save for expanded items. You can switch it on within the
+	// nib. Problem though is that the data store must be available at nib
+	// awaking time! Otherwise, how can the auto-saving of expanded items
+	// compare against existing items in order to determine whether or not they
+	// should be restored as expanded or not.
+	// There's another caveat. You need to let the bindings make the necessary
+	// connections first, before connecting the Core Data context and persistent
+	// store coordinator to the store. In other words, the store must be added
+	// last. Here is a good place. Application-did-finish-launching occurs after
+	// all nib awaking methods, after bindings have been established. Hence,
+	// when the store gets added, the outline view immediately sees the contents
+	// through its bindings. Otherwise the outline-view expanded items
+	// auto-saver cannot see the items at all.
 	[outlineView setAutosaveExpandedItems:YES];
+	
 	[outlineView registerForDraggedTypes:[NSArray arrayWithObject:kAbstractTreeNodeType]];
+	
+	// The tree controller needs a sort descriptor, sorting by "order" key. The
+	// key contains an integer value specifying the order. Although moving items
+	// around does not re-sort, initial ordering necessarily applies the given
+	// sort descriptors.
+	[treeController setSortDescriptors:[NSArray arrayWithObjects:[[[NSSortDescriptor alloc] initWithKey:@"order" ascending:YES] autorelease], nil]];
 }
 
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
@@ -57,26 +77,64 @@ static NSString *kAbstractTreeNodeType = @"AbstractTreeNodeType";
 
 #pragma mark Outline View Optional Drag & Drop Support
 
-- (BOOL)outlineView:(NSOutlineView *)ov writeItems:(NSArray *)items toPasteboard:(NSPasteboard *)pasteboard
+// write items
+- (BOOL)outlineView:(NSOutlineView *)ov
+		 writeItems:(NSArray *)items
+	   toPasteboard:(NSPasteboard *)pasteboard
 {
-	NSIndexPath *indexPath = [[items objectAtIndex:0] indexPath];
-	NSData *indexPathData = [NSKeyedArchiver archivedDataWithRootObject:indexPath];
 	[pasteboard declareTypes:[NSArray arrayWithObject:kAbstractTreeNodeType] owner:self];
-	[pasteboard setData:indexPathData forType:kAbstractTreeNodeType];
+	[pasteboard setData:[NSKeyedArchiver archivedDataWithRootObject:[[items objectAtIndex:0] indexPath]] forType:kAbstractTreeNodeType];
 	return YES;
 }
 
-- (NSDragOperation)outlineView:(NSOutlineView *)ov validateDrop:(id <NSDraggingInfo>)info proposedItem:(id)item proposedChildIndex:(NSInteger)index
+// validate drop
+- (NSDragOperation)outlineView:(NSOutlineView *)ov
+				  validateDrop:(id <NSDraggingInfo>)info
+				  proposedItem:(id)item
+			proposedChildIndex:(NSInteger)index
 {
-	return index == -1 ? NSDragOperationGeneric : NSDragOperationNone;
+	// If the proposed drop item is a descendant of the item being dragged,
+	// answer none for drag operation. You cannot do it; not in one operation at
+	// least. Only move the drag item if, and only if, the proposed target is
+	// not the item itself nor any of the proposed item's parents.
+	NSTreeNode *node = [[treeController arrangedObjects] descendantNodeAtIndexPath:[NSKeyedUnarchiver unarchiveObjectWithData:[[info draggingPasteboard] dataForType:kAbstractTreeNodeType]]];
+	for (NSTreeNode *parentNode = item; parentNode; parentNode = [parentNode parentNode])
+	{
+		if (node == parentNode) return NSDragOperationNone;
+	}
+	return NSDragOperationMove;
 }
 
-- (BOOL)outlineView:(NSOutlineView *)ov acceptDrop:(id <NSDraggingInfo>)info item:(id)item childIndex:(NSInteger)index
+// accept drop
+- (BOOL)outlineView:(NSOutlineView *)ov
+		 acceptDrop:(id <NSDraggingInfo>)info
+			   item:(id)item
+		 childIndex:(NSInteger)index
 {
-	NSIndexPath *indexPath = [NSKeyedUnarchiver unarchiveObjectWithData:[[info draggingPasteboard] dataForType:kAbstractTreeNodeType]];
-	id root = [treeController arrangedObjects];
-	NSTreeNode *node = [root descendantNodeAtIndexPath:indexPath];
-	[treeController moveNode:node toIndexPath:[[item indexPath] indexPathByAddingIndex:0]];
+	// Note, argument "index" equals NSOutlineViewDropOnItemIndex (value of
+	// minus one) when the user drops an item directly on top of another.
+	// Also note, moving a node does not rearrange the objects. If sorting
+	// descriptors apply, moving does not re-apply the descriptors
+	// automatically.
+	NSIndexPath *indexPath;
+	if (item == nil)
+	{
+		if (index == NSOutlineViewDropOnItemIndex)
+		{
+			index = [[[treeController arrangedObjects] childNodes] count];
+		}
+		indexPath = [NSIndexPath indexPathWithIndex:index];
+	}
+	else
+	{
+		if (index == NSOutlineViewDropOnItemIndex)
+		{
+			index = [[item childNodes] count];
+		}
+		indexPath = [[item indexPath] indexPathByAddingIndex:index];
+	}
+	[treeController moveNode:[[treeController arrangedObjects] descendantNodeAtIndexPath:[NSKeyedUnarchiver unarchiveObjectWithData:[[info draggingPasteboard] dataForType:kAbstractTreeNodeType]]] toIndexPath:indexPath];
+	[treeController orderRepresentedObjectsForKey:@"order"];
 	return YES;
 }
 
